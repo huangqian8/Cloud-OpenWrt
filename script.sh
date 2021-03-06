93757c897cb2422eb98094f8e094703202ec4436
huangqian8 <bluefire1982@gmail.com>
2021-03-06 22:42:47 +0800

    20210306

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-# Version: 0.0
+cd openwrt
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package-temp
+mv -f package-temp/luci-app-openclash package/lean/
+rm -rf package-temp
